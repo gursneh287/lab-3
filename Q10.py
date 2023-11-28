6ae6c398a58cfa1cecc290e515ee7bff138ae2ce
gursneh287 <144039255+gursneh287@users.noreply.github.com>
2023-11-28 18:27:43 +0530

    Update Q10.py

--- Q10.py
+++ Q10.py
@@ -1,15 +1,15 @@
 x,y,z=map(int,input().split())
-if x<y or x<z:
+if x<y and x<z:
     if y>z:
         print(y)
     else:
         print(z)
-elif y<x or y<z:
+elif y<x and y<z:
     if x>z:
         print(x)
     else:
         print(z)
-elif z<x or z<y:
+elif z<x and z<y:
     if x>y:
         print(x)
     else:
